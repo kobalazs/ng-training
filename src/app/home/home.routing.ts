60bb0aa4f929ebba705f1605e3280f6027053e91
Balázs Kovács <kobalazs@gmail.com>
2017-04-20 19:48:25 +0200

    Run `ng g component home/components/home`, configure routing to HomeComponent

--- src/app/home/home.routing.ts
+++ src/app/home/home.routing.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component'
 
 @NgModule({
-  imports: [RouterModule.forChild([])],
+  imports: [RouterModule.forChild([
+    { path: '', component: HomeComponent }
+  ])],
   exports: [RouterModule]
 })
 export class HomeRoutingModule {
