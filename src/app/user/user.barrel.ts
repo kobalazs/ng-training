f875f38570830fbe83226682dd6311d162f03de1
Balázs Kovács <kobalazs@gmail.com>
2017-04-22 16:38:34 +0200

    Run `ng g service user/services/user`, set provider, create barrel for UserModule

---
+++ src/app/user/user.barrel.ts
@@ -0,0 +1,9 @@
+// Routing
+export * from './user.routing';
+
+// Services
+export * from './services/user.service';
+
+// Components
+export * from './components/login/login.component';
+export * from './components/registration/registration.component';
