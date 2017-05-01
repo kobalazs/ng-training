8115455478b5f12b032b79fb43f62a4d0c272794
Balázs Kovács <kobalazs@gmail.com>
2017-05-01 22:22:02 +0200

    Timekeeper corrections

--- src/app/shared/decorators/timekeeper.decorator.ts
+++ src/app/shared/decorators/timekeeper.decorator.ts
@@ -1,9 +1,9 @@
 export function Timekeeper(): PropertyDecorator {
   let getGmtTime = () => {
-    return Date.now() + (new Date()).getTimezoneOffset() * 60 * 1000;
+    return Date.now() + new Date().getTimezoneOffset() * 60 * 1000;
   };
 
-  return (target: number, key: string) => {
+  return (target: any, key: string) => {
     target[key] = getGmtTime();
     setInterval(() => {
       target[key] = getGmtTime();
