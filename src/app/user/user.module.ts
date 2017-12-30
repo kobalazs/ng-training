fbe7fb6d682939a219c7dc1136a2e03c14a34283
Balázs Kovács <kobalazs@gmail.com>
2017-12-30 15:40:54 +0100

    Generate new UserModule with LoginComponent and RegistrationComponent

---
+++ src/app/user/user.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UserRoutingModule } from './user-routing.module';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    UserRoutingModule
+  ],
+  declarations: [LoginComponent, RegistrationComponent]
+})
+export class UserModule { }
