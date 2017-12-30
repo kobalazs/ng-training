0f60b677a055ab88c57c23a21ad2fc1508d0d527
Balázs Kovács <kobalazs@gmail.com>
2017-12-30 16:26:40 +0100

    Create User model & build a basic registration form

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { UserRoutingModule } from './user-routing.module';
 import { LoginComponent } from './components/login/login.component';
@@ -8,6 +9,7 @@ import { RegistrationComponent } from './components/registration/registration.co
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     UserRoutingModule
   ],
   declarations: [LoginComponent, RegistrationComponent]
