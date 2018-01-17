886d12f1cb25f6656a98112ae50d58565cdf1a86
Balázs Kovács <kobalazs@gmail.com>
2018-01-17 20:36:51 +0100

    Initial setup for E2E testing

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -7,8 +7,8 @@ describe('ng-training App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display home works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('home works!');
   });
 });
