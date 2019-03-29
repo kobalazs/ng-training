e134809288e24c398eae9f89dcce8fb5e4bc4a61
Balázs Kovács <balazs.kovacs@stylersonline.com>
2019-03-29 11:45:56 +0100

    Generate new UserModule with LoginComponent and RegistrationComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomeModule' }
+  { path: 'home', loadChildren: './home/home.module#HomeModule' },
+  { path: 'user', loadChildren: './user/user.module#UserModule' }
 ];
 
 @NgModule({
---
+++ src/app/user/user.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UserRoutingModule } from './user-routing.module';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+
+@NgModule({
+  declarations: [LoginComponent, RegistrationComponent],
+  imports: [
+    CommonModule,
+    UserRoutingModule
+  ]
+})
+export class UserModule { }
