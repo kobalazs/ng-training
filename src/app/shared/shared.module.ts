74f17b7a84178bfebd2079d32898c84aa95e4196
Balázs Kovács <balazs.kovacs@stylersonline.com>
2019-03-29 14:27:08 +0100

    Generate new NavigationComponent with [routerLink] directives imported from RouterModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
-  declarations: [NotFoundComponent],
+  declarations: [
+    NotFoundComponent,
+    NavigationComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    NavigationComponent
   ]
 })
 export class SharedModule { }
