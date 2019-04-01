05043b93cec2c2bcb71941b3d1c45ac25c81cc5c
Balázs Kovács <kobalazs@gmail.com>
2019-04-01 12:59:15 +0200

    Generate new AuthGuard, implement CanActivate interface, add to /task route; navigate to home on logout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AuthGuard } from './shared/guards/auth.guard';
 import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomeModule' },
   { path: 'user', loadChildren: './user/user.module#UserModule' },
-  { path: 'task', loadChildren: './task/task.module#TaskModule' },
+  { path: 'task', loadChildren: './task/task.module#TaskModule', canActivate: [AuthGuard] },
   { path: '**', component: NotFoundComponent }
 ];
 
