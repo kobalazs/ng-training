45ec1e15fe5e60a7bb316cbba2440757b77409cb
Balázs Kovács <kobalazs@gmail.com>
2019-04-01 11:03:14 +0200

    Implement stateful AuthService, logout action, navbar user info; redirect to home after login

--- src/app/shared/components/navigation/navigation.component.ts
+++ src/app/shared/components/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-navigation',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-  constructor() { }
+  public constructor(public authService: AuthService) {
+    //
+  }
 
-  ngOnInit() {
+  public ngOnInit() {
+    //
   }
 
 }
