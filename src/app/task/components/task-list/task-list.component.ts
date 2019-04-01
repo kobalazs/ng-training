42f0bb8381d26d5e83af826f5661e4060c0f9f76
Balázs Kovács <kobalazs@gmail.com>
2019-04-01 15:25:01 +0200

    Add loader animation, disable interactions while loading

--- src/app/task/components/task-list/task-list.component.ts
+++ src/app/task/components/task-list/task-list.component.ts
@@ -10,6 +10,7 @@ import { Task } from '../../models/task';
 })
 export class TaskListComponent implements OnInit {
 
+  public loading: boolean;
   public tasks: Task[] = [];
 
   public constructor(private taskService: TaskService) {
@@ -21,24 +22,37 @@ export class TaskListComponent implements OnInit {
   }
 
   public addNewTask() {
+    this.loading = true;
     const task = new Task();
     task.name = 'New Task';
     this.taskService.create(task).subscribe(
-      () => this.loadTasks()
+      () => this.loadTasks(),
+      () => this.loading = false
     );
   }
 
   public updateTask(task: Task) {
+    this.loading = true;
     this.taskService.update(task).subscribe(
-      updatedTask => task = updatedTask,
+      updatedTask => {
+        task = updatedTask;
+        this.loading = false;
+      },
       error => this.loadTasks()
     );
   }
 
   private loadTasks() {
+    this.loading = true;
     this.taskService.list().subscribe(
-      tasks => this.tasks = tasks,
-      error => console.log(error)
+      tasks => {
+        this.tasks = tasks;
+        this.loading = false;
+      },
+      error => {
+        console.log(error);
+        this.loading = false;
+      }
     );
   }
 
