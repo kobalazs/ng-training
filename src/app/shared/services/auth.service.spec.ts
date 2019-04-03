21479d29227da4f4dcb6bffe4ce83bbe04202c99
Balázs Kovács <balazs.kovacs@stylersonline.com>
2019-04-03 09:42:30 +0200

    Unit test of AuthService

--- src/app/shared/services/auth.service.spec.ts
+++ src/app/shared/services/auth.service.spec.ts
@@ -1,19 +1,68 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
+import { environment } from './../../../environments/environment';
 import { AuthService } from './auth.service';
+import { User } from 'src/app/user/models/user';
 
 describe('AuthService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [
-      HttpClientTestingModule,
-      RouterTestingModule
-    ]
-  }));
+  let authService: AuthService;
+  let httpMock: HttpTestingController;
+  let user: User;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule
+      ]
+    });
+    authService = TestBed.get(AuthService);
+    httpMock = TestBed.get(HttpTestingController);
+    user = {
+      id: 1,
+      name: 'Tobias Test',
+      email: 'test@example.com',
+      password: 'secret'
+    };
+  });
 
   it('should be created', () => {
     const service: AuthService = TestBed.get(AuthService);
     expect(service).toBeTruthy();
   });
+
+  it('can log in', done => {
+    authService.login(user).subscribe(
+      response => {
+        console.log(response.user);
+        expect(response.user).toEqual(user);
+        expect(response.token).toBe('someToken');
+
+        expect(localStorage.getItem('user')).toBe(JSON.stringify(user));
+        expect(localStorage.getItem('token')).toBe('someToken');
+
+        done();
+      }
+    );
+    const loginRequest = httpMock.expectOne(environment.apiEndpoint + '/auth');
+    loginRequest.flush({user, token: 'someToken'});
+  });
+
+
+  it('can log out', () => {
+    authService.user = user;
+    authService.token = 'someToken';
+    localStorage.setItem('user', JSON.stringify(user));
+    localStorage.setItem('token', 'someToken');
+
+    authService.logout();
+
+    expect(authService.user).toBeUndefined();
+    expect(authService.token).toBeUndefined();
+    expect(localStorage.getItem('user')).toBeNull();
+    expect(localStorage.getItem('token')).toBeNull();
+  });
+
 });
