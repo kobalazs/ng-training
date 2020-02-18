8a1d57ff4c8ff01ccb02a4eb5ef43ff79613b532
Balázs Kovács <kobalazs@gmail.com>
2020-02-18 23:34:09 +0100

    Generate new SharedModule without routing, generate new NotFoundComponent, set for invalid routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
+
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import(`./home/home.module`).then(m => m.HomeModule) },
   { path: 'user', loadChildren: () => import(`./user/user.module`).then(m => m.UserModule) },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
