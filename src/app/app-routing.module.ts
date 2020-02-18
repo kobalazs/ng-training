78adfde05d0ff9f5349d066cb2b36d33aeda3bac
Balázs Kovács <balazs.kovacs@stylersonline.com>
2020-02-18 14:18:56 +0100

    Run `ng generate module home --routing`, add routes to app routing module, clear up AppComponent template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', loadChildren: () => import(`./home/home.module`).then(m => m.HomeModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
