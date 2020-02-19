059e4c646e250b2d153e8a325ddc3a734e5e1970
Balázs Kovács <kobalazs@gmail.com>
2020-02-19 09:10:45 +0100

    Generate new NavigationComponent, use it in AppComponent, export the new component & import SharedModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotFoundComponent } from './components/not-found/not-found.component';
-
-
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
-  declarations: [NotFoundComponent],
+  declarations: [NotFoundComponent, NavigationComponent],
   imports: [
     CommonModule
+  ],
+  exports: [
+    NavigationComponent
   ]
 })
 export class SharedModule { }
