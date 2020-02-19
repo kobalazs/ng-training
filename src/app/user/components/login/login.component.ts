7202ae0b5a25df33268cbee91dc08c53bb5b30b4
Balázs Kovács <kobalazs@gmail.com>
2020-02-19 10:56:59 +0100

    Implement login form, generate new AuthService in SharedModule, implement login call

--- src/app/user/components/login/login.component.ts
+++ src/app/user/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { AuthService, AuthResponse } from 'src/app/shared/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public form = new FormGroup(
+      {
+        email: new FormControl('', [Validators.required, Validators.email]),
+        password: new FormControl('', [Validators.required])
+      }
+    );
 
-  ngOnInit(): void {
+  public constructor(private authService: AuthService) {
+    //
   }
 
+  public ngOnInit(): void {
+    //
+  }
+
+  public login() {
+    this.authService.login(this.form.value).subscribe(
+      (response: AuthResponse) => {
+        console.log(response);
+        window.alert('Successful login!');
+        this.form.reset();
+      },
+      (error: any) => {
+        console.log(error);
+        window.alert('Login failed.');
+      },
+      () => {
+        //
+      }
+    );
+  }
 }
