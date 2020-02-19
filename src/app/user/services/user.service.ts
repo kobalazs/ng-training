1dea201f068f69c9f6d8293f1226a3dd069c5272
Balázs Kovács <kobalazs@gmail.com>
2020-02-19 10:15:09 +0100

    Run `ng g service user/services/user` & call UserService.register() in RegistrationComponent

---
+++ src/app/user/services/user.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+import { UserDto } from '../dtos/user.dto';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor() { }
+
+  public register(userDto: UserDto) {
+    console.log('Register user:', userDto);
+  }
+}
